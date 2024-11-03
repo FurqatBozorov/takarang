e8b0174c1dab7c0ba439e1e3a9a50e28c23ecdd4
FurqatBozorov <85070150+FurqatBozorov@users.noreply.github.com>
2024-11-03 21:17:31 +0100

    Update useBoolean.ts test commit

--- libs/hooks/src/lib/useBoolean.ts
+++ libs/hooks/src/lib/useBoolean.ts
@@ -3,6 +3,8 @@ import { useMemo, useState } from 'react';
 function useBoolean(initialValue = false) {
   const [value, toggle] = useState(initialValue);
 
+  console.log('test commit')
+
   const handlers = useMemo(
     () => ({
       toggle,
